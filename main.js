ab49632549b897ff423bfadc2c17e843702827b9
Vinoshanpradeeps <89023571+Vinoshanpradeeps@users.noreply.github.com>
2022-12-13 20:12:37 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -15,3 +15,22 @@ function take_snapshot(){
 }
 console.log("ML5 version" , ml5.version);
 classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/-UJnQ1UeO/model.json',modelLoaded);
+
+function modelLoaded(){
+    console.log('Model Loded');
+}
+
+function check(){
+    img = document.getElementById('captured_image');
+    classifier.classify(img, gotResult);
+}
+
+function gotResult(error, results){
+    if (error) {
+        console.error(error);
+    } else {
+        console.log(results);
+        document.getElementById("result_object_name").innerHTML = results[0].label;
+        document.getElementById("result_object_accuracy").innerHTML = results[0].confidence;
+    }
+}
